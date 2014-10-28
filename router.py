554b91742df10157194a73861274295b24b06ea3
Avinash Moondra <avimoondra@gmail.com>
2014-10-28 18:10:44 -0400

    clean up sampling modules + add side panel instructions with disabled buttons flow

--- router.py
+++ router.py
@@ -1,8 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 @app.route('/')
-def modules_list():
+def index():
     return render_template('index.html')
 
 @app.route('/chap1/srs')
